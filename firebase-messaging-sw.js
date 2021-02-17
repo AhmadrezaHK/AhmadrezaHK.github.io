8ff6af50127a67f21dab531bc73acfd94b5f72d1
ahmadreza <ahmadrezahk23@gmail.com>
2021-02-17 11:58:09 +0330

    asdasdas

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -57,7 +57,7 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.setBackgroundMessageHandler((payload) => {
-  console.log("BackgroundMessage");
+  console.log("BackgroundMessage", payload);
   return self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
   });

70aaa7611abe718a3b93ea3fe1b45bb528de04e2
ahmadreza <ahmadrezahk23@gmail.com>
2021-02-17 10:51:20 +0330

    dsdf

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 Notification.requestPermission();
-navigator.serviceWorker.register("/sw.js");
+let registeration = navigator.serviceWorker.register("/sw.js");
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC5KQoqzq1eYFTY3TKUhpLxxz-JxbHK5Y0",
+  authDomain: "front-chapter.firebaseapp.com",
+  projectId: "front-chapter",
+  storageBucket: "front-chapter.appspot.com",
+  messagingSenderId: "406353635928",
+  appId: "1:406353635928:web:cf176a3ea8ed0d1c5df82d",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+messaging.useServiceWorker(registeration);
--- sw.js
+++ sw.js
@@ -1,42 +1,61 @@
-self.addEventListener("install", (event) => {
-  event.waitUntil(self.skipWaiting().then(() => null));
-  console.log("INSTALLED EVENT");
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(self.skipWaiting().then(() => null));
+//   console.log("INSTALLED EVENT");
+// });
+
+// self.addEventListener("activate", (event) => {
+//   console.log("ACTIAVTE EVENT");
+// });
+
+// self.addEventListener("push", (event) => {
+//   console.log("PUSH EVENT", event);
+//   self.registration.showNotification("notification title", {
+//     body: "notification body",
+//     requireInteraction: true,
+//     actions: [
+//       {
+//         title: "yesss",
+//         action: "A",
+//       },
+//       {
+//         title: "noooo",
+//         action: "B",
+//       },
+//     ],
+//   });
+// });
+
+// self.addEventListener("notificationclick", (event) => {
+//   console.log("NOTIFICATION CLICK EVENT", event.action);
+//   event.notification.close();
+//   if (event.action === "A") {
+//     self.clients.openWindow("https://google.com");
+//   } else if (event.action === "B") {
+//     self.clients.openWindow("https://wikipedia.com");
+//   } else {
+//     self.clients.openWindow("https://dev.to");
+//   }
+// });
+
+// self.addEventListener("pushsubscriptionchange", (event) => {
+//   console.log("subs changed", event);
+// });
+
+importScripts("https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.7/firebase-messaging.j");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC5KQoqzq1eYFTY3TKUhpLxxz-JxbHK5Y0",
+  authDomain: "front-chapter.firebaseapp.com",
+  projectId: "front-chapter",
+  storageBucket: "front-chapter.appspot.com",
+  messagingSenderId: "406353635928",
+  appId: "1:406353635928:web:cf176a3ea8ed0d1c5df82d",
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("ACTIAVTE EVENT");
-});
+// eslint-disable-next-line no-undef
+const messaging = firebase.messaging();
 
-self.addEventListener("push", (event) => {
-  console.log("PUSH EVENT", event);
-  self.registration.showNotification("notification title", {
-    body: "notification body",
-    requireInteraction: true,
-    actions: [
-      {
-        title: "yesss",
-        action: "A",
-      },
-      {
-        title: "noooo",
-        action: "B",
-      },
-    ],
-  });
-});
-
-self.addEventListener("notificationclick", (event) => {
-  console.log("NOTIFICATION CLICK EVENT", event.action);
-  event.notification.close();
-  if (event.action === "A") {
-    self.clients.openWindow("https://google.com");
-  } else if (event.action === "B") {
-    self.clients.openWindow("https://wikipedia.com");
-  } else {
-    self.clients.openWindow("https://dev.to");
-  }
-});
-
-self.addEventListener("pushsubscriptionchange", (event) => {
-  console.log("subs changed", event);
+messaging.onBackgroundMessage((payload) => {
+  console.log("asdfsfsdafsd");
 });
